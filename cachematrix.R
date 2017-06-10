075d3f620c70b90dc9d7d91e734cadd10f0ad87c
YileXingCoursera <xing_16@sina.com>
2017-06-10 18:12:16 +0800

    Create cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function calculates the inverse of a square matrix
 
-## Write a short comment describing this function
-
+## This function returns a list that contains a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-Null
+  set<-function(y){
+          x<<-y
+          m<<-Null
+  }
+  get<-function(){
+          x
+  }
+  setinverse<-function(solve){
+          m<<-solve
+  }
+  getinverse<-function(){
+          m
+  }
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of a square matrix x
+## If x is not a square matrix, then a message and number 0 return
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getinverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        if(nrow(data)==ncol(data)){
+                m<-solve(data,...)
+                x$setinverse(m)
+                m
+        }else{
+                message("wrong matrix")
+                return(0)
+        }        
 }
